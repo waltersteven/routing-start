9497b0e243c6c4be0f24eca4c13cecbb054309e1
Walter Pariona <wltrst97@gmail.com>
2017-12-14 00:26:21 -0500

    Passing parameters to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'servers', component: ServersComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent }, // ":" indicates its a dinamic part.
 ];
 @NgModule({
   declarations: [
