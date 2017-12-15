5231ecf50f77f19a593a463b05289405d1a8f492
Walter Pariona <wltrst97@gmail.com>
2017-12-14 23:20:42 -0500

    Practicing and some common gotchas

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -10,10 +10,17 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id']; // + is to convert it to a number
+    this.server = this.serversService.getServer(id);
+
+    this.route.params.subscribe( // params is an observable: feature that allows us to work with asynchronous task, use this when we will update the component from inside, it will subscribe to an event which might happen in the future.
+      (params: Params) =>  { //get executed whenever the parameter changes
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
   }
 
 }
